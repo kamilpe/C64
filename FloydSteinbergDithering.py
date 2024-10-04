9eed31dac7fd4e24d4bd22a0dd244db1277be332
Kamil Pawlowski <kamilpe@gmail.com>
2024-10-04 20:27:37 +0200

    Dithering is working :)

--- FloydSteinbergDithering.py
+++ FloydSteinbergDithering.py
@@ -1,8 +1,30 @@
+import math
 from ColorTools import find_closest
 
-def apply_dithering(image, width, height, palette):
+def apply_dithering(image, width, height, palette, dithering = True):
+    info_step = height/16
+    next_info_step = 0
+
     for y in range(0, height):
         for x in range(0, width):
-            image[y,x] = palette[find_closest(image[y,x], palette)]
-            
+            old = image[y, x]
+            new = palette[find_closest(old, palette)]
+
+            if (dithering):
+                error = old - new
+                if (x + 1 < width):
+                    image[y, x + 1] += error * 0.4375 # right, 7 / 16
+                if (y + 1 < height) and (x + 1 < width):
+                    image[y + 1, x + 1] += error * 0.0625 # right, down, 1 / 16
+                if (y + 1 < height):
+                    image[y + 1, x] += error * 0.3125 # down, 5 / 16
+                if (x - 1 >= 0) and (y + 1 < height): 
+                    image[y + 1, x - 1] += error * 0.1875 # left, down, 3 / 16
+
+            image[y,x] = new
+        
+        if ((y+1) >= next_info_step):
+            next_info_step += info_step
+            print(str(math.floor((y+1)*100/height)) + '%')
+
     return image
--- convert.py
+++ convert.py
@@ -2,12 +2,12 @@ import C64Palette
 import numpy
 import math
 from FloydSteinbergDithering import apply_dithering
-from ColorTools import rgb_to_xyz, xyz_to_lab, lab_to_xyz, xyz_to_rgb
+from ColorTools import xyz_to_lab, rgb_to_xyz, xyz_to_rgb, lab_to_xyz
 from PIL import Image
 
 C64Palette.init_palettes()
 
-img_input_file = "input.jpg"
+img_input_file = "IMG_4323.jpeg"
 img_output_file = "output.png"
 print('Reading', img_input_file)
 with Image.open(img_input_file) as pic:
@@ -18,20 +18,21 @@ with Image.open(img_input_file) as pic:
     pic_cs = numpy.empty((pic.height, pic.width, 3), dtype=numpy.float32)
     for y in  range(0, pic.height):
         for x in range(0, pic.width):
-            pic_cs[y,x] = xyz_to_lab(rgb_to_xyz(pic_array[y,x]))
-
-#    print('Applying palette...')
-#    for y in  range(0, pic.height):
-#        for x in range(0, pic.width):
-#            pic_cs[y,x] = palette[find_closest(pic_cs[y,x], palette)]
-
+            if (palette == C64Palette.RGB):
+                pic_cs[y,x] = pic_array[y,x]
+            else:
+                pic_cs[y,x] = xyz_to_lab(rgb_to_xyz(pic_array[y,x]))
+            
     print('Applying dithering...')
-    apply_dithering(pic_cs, pic.width, pic.height, palette)
+    apply_dithering(pic_cs, pic.width, pic.height, palette, True)
 
-    print('Converting to 32bit RGB')
+    print('Converting color space to the 32bit RGB')
     for y in  range(0, pic.height):
         for x in range(0, pic.width):
-            pic_array[y,x] = xyz_to_rgb(lab_to_xyz(pic_cs[y,x]))
+            if (palette == C64Palette.RGB):
+                pic_array[y,x] = pic_cs[y,x]
+            else:
+                pic_array[y,x] = xyz_to_rgb(lab_to_xyz(pic_cs[y,x]))
 
     print('Saving', img_output_file)
     Image.fromarray(pic_array).save(img_output_file)
